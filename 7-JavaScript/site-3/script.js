58e768f344cefab3e02a799d8b78f4323a0a811f
lucasdias6 <4555323113@estudante.sed.sc.gov.br>
2026-08-04 17:02:36 -0300

    Introdução ao JavaScript

--- 7-JavaScript/site-3/script.js
+++ 7-JavaScript/site-3/script.js
@@ -3,8 +3,8 @@ const subtitulo = document.getElementById("subtitulo");
 const mensagem = document.getElementById("mensagem");
 
 titulo.innerHTML = "EXÉRCITO DE SWEETIE FOX ATRÁS DE MIM!!"
-titulo.style.color = "RED"
 subtitulo.innerHTML = "CONSEGUI ESCAPAR??"
-subtitulo.style.color = "blue"
 mensagem.innerHTML = "ENTENDA O CASO!!"
 
+titulo.style.color = "RED"
+subtitulo.style.color = "blue"
