5f21e424c45988706e1db453c647da00f9b9380f
lucasdias6 <4555323113@estudante.sed.sc.gov.br>
2026-09-01 14:46:00 -0300

    Teoria JS e atualização no site 11

--- 7-JavaScript/site-11/script.js
+++ 7-JavaScript/site-11/script.js
@@ -2,9 +2,23 @@ const tarefa = document.getElementById('input-tarefa');
 const btn = document.getElementById('btn-adicionar');
 const lista = document.getElementById('lista-tarefas');
 
+
 btn.addEventListener('click', function(){
     
     if (tarefa.value != "") {
+        const novoItem = document.createElement('li');
+        novoItem.textContent = tarefa.value
+        
+        lista.appendChild(novoItem);
+        
+        tarefa.value = "";
+        tarefa.focus();
+    }
+})
+
+tarefa.addEventListener('keydown', function(event) {
+    
+    if (tarefa.value != "" & event.key === "Enter") {
         const novoItem = document.createElement('li');
         novoItem.textContent = tarefa.value
 
